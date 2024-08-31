afe897b5d5885a7f21c2cf88f54578baeb975e9e
shresth <shrestha.tripathi@gmail.com>
2024-08-31 14:06:04 +0530

    added icard

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from '../style';
 import { services } from '../constants';
 import { fadeIn, textVariant } from "../utils/motion";
+import { Card } from "./canvas";
 
 import { SectionWrapper } from '../hoc';
 
@@ -45,6 +46,7 @@ const About = () => {
         <p className={`${styles.sectionSubText}`}>Introduction</p>
         <h2 className={`${styles.sectionHeadText}`}>Overview.</h2>
       </motion.div>
+      <div className='flex'>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
@@ -61,6 +63,10 @@ const About = () => {
         🔹 Web development expertise: Creating intuitive user interfaces and seamless user experiences. <br/>
         🔹 Passionate about ethical hacking and staying ahead of emerging cybersecurity threats. <br/>
       </motion.p>
+      <div>
+        <Card />
+      </div>
+      </div>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard
